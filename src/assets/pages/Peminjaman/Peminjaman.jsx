3739d5b5eb07da1b7a13c6b5ac97cffe1b51bc4e
RafaelRusdianto <230712325@students.uajy.ac.id>
2025-11-13 22:15:04 +0700

    first commit peminjaman.jsx

--- src/assets/pages/Peminjaman/Peminjaman.jsx
+++ src/assets/pages/Peminjaman/Peminjaman.jsx
@@ -2,3 +2,24 @@ import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "./Peminjaman.css";
+
+export default function Peminjaman() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [book, setBook] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:8000/api/buku/${id}`)
+            .then((res) => res.json())
+            .then((data) => setBook(data.data))
+            .catch(() => toast.error("Failed to load book data"));
+    }, [id]);
+
+    if (!book) return <p>Loading borrowing details...</p>;
+
+    return (
+        <div className="peminjaman-container">
+
+        </div>
+    );
+}
